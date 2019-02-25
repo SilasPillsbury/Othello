53b04937154ac5b8aa0ff581ba2067231a75d25b
SilasPillsbury <30637327+SilasPillsbury@users.noreply.github.com>
2019-02-25 11:10:15 -0500

    First Commit
    Added board, and beginning of check_playable function

--- Othello.py
+++ Othello.py
@@ -1,7 +1,20 @@
-b =[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],  [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+b =[[0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]]
 def play(b):
     'Things'
     Return None
 
-def Coward(b,playstate):
-    return 'Awesome'
+def check_playable(player,location,board):
+    if b[location[0]][location[1]] != 0:
+        return False
+    for x in board[location[0]]:
+        
+        
+    for entry in range(8):
+        y = board[entry][location[1]]
