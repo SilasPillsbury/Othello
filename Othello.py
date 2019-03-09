20283528004ca56d121579de362c6cf69f1d79fb
SilasPillsbury <30637327+SilasPillsbury@users.noreply.github.com>
2019-03-09 17:19:02 -0500

    Addition to check_playable function
    now checks to the right and below of location for tokens

--- Othello.py
+++ Othello.py
@@ -6,15 +6,36 @@ b =[[0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0]]
+
 def play(b):
     'Things'
-    Return None
+    return None
 
 def check_playable(player,location,board):
+    """
+    Player - int 1 or 2, location - length 2 index,
+    board - current board state, has nested indexes
+    """
+    playable = False
+    
     if b[location[0]][location[1]] != 0:
-        return False
-    for x in board[location[0]]:
-        
-        
-    for entry in range(8):
+        return playable
+        #performs check if spot is empty
+    
+    for x in board[location[0]][location[1]+1:8]:
+        if x == abs(3-player):
+            playable = True
+            continue
+        if x == 0:
+            playable = False
+            break
+        if x == player:
+            break
+
+    if playable:
+        return playable
+    
+    for entry in range(8-location[1]):
         y = board[entry][location[1]]
+
+    
